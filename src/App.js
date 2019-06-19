796b01887f79d80df0268cb1621817355c826ae9
Daniel Velazquez Giron <danip293@gmail.com>
2019-06-19 12:16:40 -0500

    se corrigio el navbar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
-import { CouponFormContainer } from './containers/CouponFormContainer';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import {
+  CouponFormContainer,
+  UpdateCouponContainer,
+} from './containers/CouponFormContainer';
 import CouponList from './containers/Cuponera';
 import { QrReaderContainer } from './containers/QrReaderContainer';
 function App() {
@@ -13,29 +16,31 @@ function App() {
         <div className="App">
           <Navbar color="light" light expand="md">
             <NavbarBrand href="/">
-              {' '}
               <img src={logo} className="App-logo" alt="logo" />
               reactstrap
             </NavbarBrand>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <Link to="/crear_cupon"> Crear Cupon </Link>
+                <NavLink>
+                  <Link to="/crear_cupon"> Crear Cupon </Link>
+                </NavLink>
               </NavItem>
               <NavItem>
-                <Link to="/administrar_cupones">Lista de cupones</Link>
+                <NavLink>
+                  <Link to="/administrar_cupones">Lista de cupones</Link>
+                </NavLink>
               </NavItem>
               <NavItem>
-                <Link to="/detallesCupon">Detalles del cupón</Link>
-              </NavItem>
-              <NavItem>
-                <Link to="/lectorQR">lectorQR</Link>
+                <NavLink>
+                  <Link to="/lectorQR">lectorQR</Link>
+                </NavLink>
               </NavItem>
             </Nav>
           </Navbar>
           <Route path="/crear_cupon" component={CouponFormContainer} />
           <Route path="/administrar_cupones" component={CouponList} />
           <Route path="/lectorQR" component={QrReaderContainer} />
-          {/*<Route path="/detail/:id" component={} />*/}
+          <Route path="/detail/:id" component={UpdateCouponContainer} />
         </div>
       </Router>
     </div>
