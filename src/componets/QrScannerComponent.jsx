8f7e66ab985c0c6c1e2634932778a5bcff1f9f9f
Daniel Velazquez Giron <danip293@gmail.com>
2019-06-19 10:50:26 -0500

    se creo el contenedor y el componente para leer codigos qr

---
+++ src/componets/QrScannerComponent.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import QrReader from 'react-qr-reader';
+
+import { Container, Row, Col, CardBody, Card } from 'reactstrap';
+
+export class QrScannerComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleScan = this.handleScan.bind(this);
+    this.handleError = this.handleError.bind(this);
+    this.toggle = this.toggle.bind(this);
+
+    this.state = {
+      currentQR: '',
+      scannerError: '',
+    };
+  }
+
+  handleScan(data) {
+    if (data && data !== this.state.currentQR) {
+      window.open(data);
+      this.setState({
+        currentQR: data,
+      });
+    }
+  }
+
+  handleError() {
+    this.setState({
+      currentQR: 'Ocurrio un error',
+    });
+  }
+
+  toggle() {
+    this.setState(state => ({ currentQR: !state.currentQR }));
+  }
+
+  render() {
+    return (
+      <Row className="justify-content-center mb-5">
+        <Col xs="12" lg="7">
+          <Card>
+            <CardBody>
+              <QrReader
+                delay={300}
+                onError={this.handleError}
+                onScan={this.handleScan}
+                style={{ width: '100%' }}
+              />
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
+}
---
+++ src/containers/QrReaderContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+export const QrReaderContainer = props => {
+  if (typeof window !== 'undefined') {
+    const QrScannerComponent = require('../componets/QrScannerComponent')
+      .QrScannerComponent; // eslint-disable-line global-require
+    return (
+      <Container>
+        <QrScannerComponent {...props} />
+      </Container>
+    );
+  }
+  return null;
+};
