0a77defce8d7d734f3b5c2ce4876aa2f18b16c58
Daniel Velazquez Giron <danip293@gmail.com>
2019-06-21 15:15:32 -0500

    se agrego qrcode.react y react-qr-code al packaje.json

--- src/containers/CouponPublicListContainer.jsx
+++ src/containers/CouponPublicListContainer.jsx
@@ -1,100 +1,29 @@
 import React from 'react';
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  CardTitle,
-  CardSubtitle,
-  CardBody,
-  CardText,
-  CardImg,
-  Button,
-  Badge,
-  CardFooter,
-  CardHeader,
-} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import { connect } from 'react-redux';
 
+import { CouponItemComponent } from '../componets/CouponItemComponent';
 import { getCouponsList } from '../actions/coupons';
 
 class CouponPublicListComponent extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
-    const { getCouponsList } = this.props;
-    getCouponsList();
+    this.props.getCouponsList();
   }
   render() {
-    const { couponsList } = this.props;
+    const { couponsList, history } = this.props;
     console.log(couponsList);
     return (
       <Container>
         <h2>Listado de cupones</h2>
         <Row>
           {couponsList.map(item => (
-            <Col key={item._id.$oid}>
-              <Card>
-                {item.image && (
-                  <CardImg
-                    top
-                    width="100%"
-                    src={item.image}
-                    alt="Card image cap"
-                  />
-                )}
-
-                <CardBody>
-                  <CardTitle>
-                    <h2>{item.name}</h2>
-                  </CardTitle>
-                  <CardText>{item.description}</CardText>
-                  <hr />
-                  <Row>
-                    <Col>
-                      <h2>
-                        <Badge color="info">
-                          <strike>${item.list_price} </strike>
-                        </Badge>
-                      </h2>
-                    </Col>
-                    <Col>
-                      <h2>
-                        <Badge color="danger">
-                          %{item.discount_percenatage}
-                        </Badge>
-                      </h2>
-                    </Col>
-                    <Col>
-                      <h2>
-                        <Badge color="success">
-                          {item.discount_percenatage
-                            ? `-${item.discount_percenatage}%`
-                            : ` -${item.discount_price}$`}
-                        </Badge>
-                      </h2>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <Row>
-                    <Col style={{ textAlign: 'center' }}>
-                      Empieza
-                      <div>
-                        <h4> {item.valid_sice}</h4>
-                      </div>
-                    </Col>
-                    <Col style={{ textAlign: 'center' }}>
-                      Termina
-                      <div>
-                        <h4>{item.valid_until}</h4>
-                      </div>
-                    </Col>
-                  </Row>
-                </CardFooter>
-              </Card>
+            <Col
+              key={item._id.$oid}
+              onClick={() => {
+                history.push(`/detallePublico/${item._id.$oid}`);
+              }}
+            >
+              <CouponItemComponent item={item} />
             </Col>
           ))}
         </Row>
